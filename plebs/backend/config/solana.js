c6212d46ecf6700716fb106d1bbc011e658032a4
DigiRoninCollective <129343687+DigiRoninCollective@users.noreply.github.com>
2025-06-22 23:11:43 +0000

    Add placeholder solana.js config

---
+++ plebs/backend/config/solana.js
@@ -0,0 +1,20 @@
+const { 
+  Transaction, 
+  SystemProgram, 
+  LAMPORTS_PER_SOL,
+  sendAndConfirmTransaction,
+  PublicKey,
+  ComputeBudgetProgram
+} = require('@solana/web3.js');
+
+class SolanaService {
+  constructor() {
+    // constructor implementation
+  }
+
+  someMethod() {
+    // method implementation
+  }
+}
+
+module.exports = new SolanaService();
